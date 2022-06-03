622107e6cfeb5c0735160ab0fb6cdf149c76ff01
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-06-03 12:40:38 +0200

    removed line causing error

--- nn_common_modules/modules.py
+++ nn_common_modules/modules.py
@@ -523,7 +523,8 @@ class OctaveDenseBlock(nn.Module):
 
         elif se_block_type == se.SELayer.CSSE.value:
             self.SELayer = se.ChannelSpatialSELayer(params['num_filters'])
-        else:abdominal_segmentation_2] - 1) / 2)
+            
+        
         padding_w = int((params['kernel_w'] - 1) / 2)
 
         conv1_out_size = int(params['num_channels'] + params['num_filters'])
